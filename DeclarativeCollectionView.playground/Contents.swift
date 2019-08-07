8be54f0a797cd302aa3bdfa36e52439b0cc6620e
Guihal Gwenn <gguihal@oui.sncf>
2019-08-07 09:56:50 +0200

    clean code

--- DeclarativeCollectionView.playground/Contents.swift
+++ DeclarativeCollectionView.playground/Contents.swift
@@ -1,78 +1,5 @@
 import Foundation
 
-/**protocol Adaptable {}
-
-protocol Descriptable {
-    associatedtype Adapter: Adaptable
-    var adapter: Adapter { get }
-}
-
-protocol LabelAdapterProtocol : Adaptable {}
-struct AnyLabelAdapter: LabelAdapterProtocol {}
-
-struct FooDescriptor: Descriptable {
-    var adapter = AnyLabelAdapter()
-}
-
-protocol ImageAdapterProtocol : Adaptable {
-    var key: String { get }
-}
-struct AnyImageAdapter: ImageAdapterProtocol {
-    let key = "hello"
-}
-struct BarDescriptor: Descriptable {
-    let adapter = AnyImageAdapter()
-}
-
-let bar = BarDescriptor()
-bar.adapter.key
-
-/**
- struct AnyPokemon<P: Pokemon>: Pokemon {
-     
-     private let pokemon: P
-     
-     init(_ pokemon: P) {
-         self.pokemon = pokemon
-     }
-     
-     func attack() -> P.Power {
-         return pokemon.attack()
-     }
- }
- 
- struct AnyPokemon<Power>: Pokemon {
-     
-     // we have to save the properties and functions
-     // that we need to use for the Pokemon protocol
-     private let _attack: () -> Power
-     
-     init<P: Pokemon where P.Power == Power>(_ pokemon: P) {
-         _attack = pokemon.attack
-     }
-     
-     func attack() -> Power {
-         // using the original Pokemon's attack implementation
-         return _attack()
-     }
- }
- */
-
-struct AnyDescriptor<Adapter: Adaptable>: Descriptable {
-    private let _adapter: Adapter
-    var adapter: Adapter {
-        return _adapter
-    }
-    
-    init<T: Descriptable>(_ descriptable: T) where T.Adapter == Adapter {
-        _adapter = descriptable.adapter
-    }
-    
-    
-}
-
-let descriptors: [AnyDescriptor] = [AnyDescriptor(BarDescriptor()),AnyDescriptor(FooDescriptor())]*/
-
 protocol Content {}
 
 protocol Cell: Content {
@@ -81,22 +8,18 @@ protocol Cell: Content {
 protocol Space: Content {
 }
 
-protocol Section {
-    var cells: [Cell] { get }
-    var spaces: [Space] { get }
-    var decorations: [Decoration] { get }
-    init(cells: [Cell], spaces: [Space], decorations: [Decoration])
-}
-
-protocol DecorationType {}
-
 struct Decoration: Content {
     let type: DecorationType
     let contents: [Content]
 }
 
-enum AnyDecorationType: DecorationType {
-    case ticket
+protocol DecorationType {}
+
+protocol Section {
+    var cells: [Cell] { get }
+    var spaces: [Space] { get }
+    var decorations: [Decoration] { get }
+    init(cells: [Cell], spaces: [Space], decorations: [Decoration])
 }
 
 @_functionBuilder
@@ -134,6 +57,10 @@ extension Section {
     }
 }
 
+enum AnyDecorationType: DecorationType {
+    case ticket
+}
+
 struct AnySection: Section {
     var cells: [Cell]
     var spaces: [Space]
@@ -155,7 +82,7 @@ let section = AnySection {
 }
 print(section.cells) // 3 cells
 print(section.spaces) // 1 space
-print(section.decorations) // 1 space
+print(section.decorations) // 1 decorations with n spaces and n cells
 
 
 
