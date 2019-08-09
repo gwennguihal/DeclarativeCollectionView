31392e493773573c2562d42a8ec1fe5283c83c65
Guihal Gwenn <gguihal@oui.sncf>
2019-08-09 09:21:47 +0200

    add indices + section UI

--- DeclarativeCollectionView.playground/Contents.swift
+++ DeclarativeCollectionView.playground/Contents.swift
@@ -1,31 +1,70 @@
 import Foundation
+import SwiftUI
 
 protocol Content {}
 
 protocol Cell: Content {
 }
 
-protocol Space: Content {
+struct Space: Content {
 }
 
 struct Decoration: Content {
     let type: DecorationType
     let contents: [Content]
+    
+    var startIndex: Int?
+    var endIndex: Int? {
+        guard let startIndex = startIndex else {
+            return nil
+        }
+        var count = 0
+        computeEndIndex(contents, &count)
+        return startIndex + count
+    }
+    
+    private func computeEndIndex(_ contents: [Content], _ count: inout Int) {
+        contents.forEach {
+            if $0 is Cell {
+                count += 1
+            } else if let decoration = $0 as? Decoration {
+                computeEndIndex(decoration.contents, &count)
+            }
+        }
+    }
 }
 
 protocol DecorationType {}
 
 protocol Section {
-    var cells: [Cell] { get }
-    var spaces: [Space] { get }
-    var decorations: [Decoration] { get }
-    init(cells: [Cell], spaces: [Space], decorations: [Decoration])
+    var cells: [Int: Cell] { get }
+    var spaces: [Int: Space] { get }
+    var decorations: [Int: Decoration] { get }
+    init(cells: [Int: Cell], spaces: [Int: Space], decorations: [Int: Decoration])
 }
 
 @_functionBuilder
 class ContentBuilder {
     static func buildBlock(_ contents: Content...) -> [Content] {
         return contents
+    }
+}
+
+class DataSource {
+    var sections = [Section]()
+}
+
+@_functionBuilder
+class DataSourceBuilder {
+    static func buildBlock(_ sections: Section...) -> [Section] {
+        return sections
+    }
+}
+
+extension DataSource {
+    convenience init(@DataSourceBuilder _ builder: () -> [Section]) {
+        self.init()
+        self.sections = builder()
     }
 }
 
@@ -36,24 +75,36 @@ extension Decoration {
 }
 
 extension Section {
+    
     init(@ContentBuilder _ builder: () -> [Content]) {
+        
         let contents = builder()
-        var cells = [Cell]()
-        var spaces = [Space]()
-        var decorations = [Decoration]()
-        contents.forEach {
-            switch $0 {
-            case let cell as Cell:
-                cells.append(cell)
+        var cells = [Int: Cell]()
+        var spaces = [Int: Space]()
+        var decorations = [Int: Decoration]()
+        var index = 0
+        
+        Self.invalidate(contents: contents, index: &index, cells: &cells, spaces: &spaces, decorations: &decorations)
+        
+        self.init(cells: cells, spaces: spaces, decorations: decorations)
+    }
+    
+    private static func invalidate(contents: [Content], index: inout Int, cells: inout [Int: Cell], spaces: inout [Int: Space], decorations: inout [Int: Decoration]) {
+        contents.forEach { content in
+            switch content {
             case let space as Space:
-                spaces.append(space)
-            case let decoration as Decoration:
-                decorations.append(decoration)
+                spaces[index - 1] = space
+            case var decoration as Decoration:
+                decoration.startIndex = index - 1
+                decorations[index - 1] = decoration
+                invalidate(contents: decoration.contents, index: &index, cells: &cells, spaces: &spaces, decorations: &decorations)
+            case let cell as Cell:
+                cells[index] = cell
+                index += 1
             default:
                 break
             }
         }
-        self.init(cells: cells, spaces: spaces, decorations: decorations)
     }
 }
 
@@ -63,31 +114,64 @@ enum AnyDecorationType: DecorationType {
 }
 
 struct AnySection: Section {
-    var cells: [Cell]
-    var spaces: [Space]
-    var decorations: [Decoration]
+    var cells: [Int: Cell]
+    var spaces: [Int: Space]
+    var decorations: [Int: Decoration]
 }
 
 struct AnyCell: Cell {}
-struct AnySpace: Space {}
 
-let section = AnySection {
-    AnyCell()
-    AnyCell()
-    AnySpace()
-    AnyCell()
-    Decoration(type: AnyDecorationType.ticket) {
-        AnySpace()
+struct ForEach {
+    var collection: [Any]
+}
+
+let dataSource = DataSource {
+
+    AnySection {
+        AnyCell() // 0
+        AnyCell() // 1
+        Space() // 1
+        AnyCell() // 2
+        Space() // 2
+        Decoration(type: AnyDecorationType.ticket) { //2 - 4
+            Space() // 2
+            AnyCell() // 3
+            Decoration(type: AnyDecorationType.form) { //3 - 4
+                Space() // 3
+                AnyCell() // 4
+            }
+        }
+    }
+    AnySection {
         AnyCell()
-        Decoration(type: AnyDecorationType.form) {
-            AnySpace()
-            AnyCell()
+        Space()
+    }
+    
+}
+
+// Build
+dataSource.sections.forEach { section in
+    var decorations = [Decoration]()
+    (-1..<section.cells.count).forEach { index in
+        
+        if let _ = section.cells[index] {
+            print("\(index) : cell")
+        }
+        
+        if let decoration = section.decorations[index] {
+            print("\(index) : start Decoration \(decoration.type)")
+            decorations.append(decoration)
+        }
+        
+        if let _ = section.spaces[index] {
+            print("\(index) : space")
+        }
+        
+        decorations.filter { $0.endIndex == index }.forEach { decoration in
+            print("\(index) : end Decoration \(decoration.type)")
         }
     }
 }
-print(section.cells) // 3 cells
-print(section.spaces) // 1 space
-print(section.decorations) // 1 decorations with n spaces and n cells
 
 
 
